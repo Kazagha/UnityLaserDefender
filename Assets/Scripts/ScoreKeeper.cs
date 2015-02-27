7e9953883a2c8ac9205063b1074f178390164fc2
Kazagha <chrisg.local@gmail.com>
2015-02-27 13:25:30 +1000

    ScoreKeeper
    Initial commit.

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	private int scoreInt;
+	private Text scoreText;
+
+	void Start(){
+		scoreText = this.GetComponent<Text>();
+		ResetScore();
+	}
+
+	public void AddScore(int pts){
+		scoreInt += pts;
+		UpdateText();
+	}
+	
+	public void ResetScore(){
+		scoreInt = 0;
+		UpdateText();
+	}
+	
+	private void UpdateText(){
+		scoreText.text = "Score: ";
+	}
+}
