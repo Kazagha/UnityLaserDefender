ec2532cfe2880e5f436be30d5ca37efe601efca8
Kazagha <chrisg.local@gmail.com>
2015-03-12 20:00:43 +1000

    ScoreDisplay
    Create script to fetch the score when the 'lose' screen is displayed.

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplaf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Text myText = GetComponent<Text>();
+		myText.text = ScoreKeeper.scoreInt.ToString();
+		ScoreKeeper.ResetScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
