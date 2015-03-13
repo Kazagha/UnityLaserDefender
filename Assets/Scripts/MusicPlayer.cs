07e32ddab2cab9e2680f5fda0a238f9f19d74735
Kazagha <chrisg.local@gmail.com>
2015-03-13 12:35:40 +1000

    Music Player
    Add support for multiple tracks in the music player.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip loseClip;
+
+	private AudioSource music;
 /*
 	static GameObject musicPlayerObject = null;
 
@@ -36,7 +41,33 @@ public class MusicPlayer : MonoBehaviour {
 			Debug.Log("Music Player is NULL, creating static variable");			
 			playerInstance = this;
 			GameObject.DontDestroyOnLoad(playerInstance);
+			music = this.GetComponent<AudioSource>();
+
+			music.clip = startClip;
+			music.Play();
+			music.loop = true;
 		}
+	}
+
+	void OnLevelWasLoaded(int level){
+		// Stop the current track
+		music.Stop();
+
+		// Select the apropriate track
+		if(level == 0)
+		{
+			music.clip = startClip;
+		} else if (level == 1) {
+			music.clip = gameClip;
+		} else if (level == 2) {
+			music.clip = loseClip;
+		}
+
+		// Begin playing the music
+		music.Play();
+
+		// Allow the music to continue to loop
+		music.loop = true;
 	}
 	
 	// Use this for initialization
