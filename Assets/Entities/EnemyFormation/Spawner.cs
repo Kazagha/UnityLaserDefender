fc175f90294b73bff6bb0becbd872e0c80e6f4b6
Kazagha <chrisg.local@gmail.com>
2015-03-16 08:03:04 +1000

    Spawner
    Add logic to check when the current wave has been destroyed and create a
    new wave.

--- Assets/Entities/EnemyFormation/Spawner.cs
+++ Assets/Entities/EnemyFormation/Spawner.cs
@@ -5,23 +5,29 @@ public class Spawner : MonoBehaviour {
 
 	public EnemyFormation[] formation;
 
+	EnemyFormation wave;
 	private float padding = 0.5f;
 	private int count;
 
 	// Use this for initialization
 	void Start () {
 		count = 0;
-
 		SpawnNext();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Check if all members of the formaiton have been destroyed
+		if(wave.AllMembersDead())
+		{
+			// Create a delay to avoid double spawning.
+			//Invoke("SpawnNext", 3f);
+			SpawnNext();
+		}
 	}
 
 	void SpawnNext(){
-		EnemyFormation form = formation[count++] as EnemyFormation;
+		EnemyFormation tempWave = formation[count++] as EnemyFormation;
 		
 		// Find the main camera
 		Camera camera = Camera.main;
@@ -30,8 +36,8 @@ public class Spawner : MonoBehaviour {
 		float distance = transform.position.z - camera.transform.position.z;
 		
 		// Find the Y Max
-		float yMax = camera.ViewportToWorldPoint(new Vector3(1, 1, distance)).y - (form.height * 0.5f) - padding;
+		float yMax = camera.ViewportToWorldPoint(new Vector3(1, 1, distance)).y - (tempWave.height * 0.5f) - padding;
 		
-		Instantiate (formation[0], new Vector3(0, yMax, 0), Quaternion.identity);
+		wave = Instantiate (tempWave, new Vector3(0, yMax, 0), Quaternion.identity) as EnemyFormation;
 	}
 }
